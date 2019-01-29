60799fafe91819c053150bd64240a39f1187f76c
Cody Yantis <cyantis@gmail.com>
2019-01-29 01:32:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,14 +8,14 @@ end
 
 def monopoly
 	monopoly = {:railroads => {}}
-
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads =>
-		{pieces: 4},
-		{:names => {}},
-		{:rent_in_dollars => {}}
+	monopoly = {:railroads => {
+		pieces: 4,
+		:names => {},
+		:rent_in_dollars => {}
+		}
 	}
 end
 
