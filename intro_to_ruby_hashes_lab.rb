17bc77ce528d68571a4497aa843a1a2d5fff6601
Cody Yantis <cyantis@gmail.com>
2019-01-29 01:38:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ def monopoly_with_fourth_tier
 		:names => {
 			:reading_railroad => {},
 			:pennsylvania_railroad => {},
-			:b_and_o_railroad => {}
+			:b_and_o_railroad => {},
 			:shortline => {}
 		},
 		:rent_in_dollars => {
